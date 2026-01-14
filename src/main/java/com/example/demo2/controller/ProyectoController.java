6e58dc01e322f9c39abd5abc84db14e8412aadb3
Irenecg22 <ireneca032006@gmail.com>
2026-01-14 19:50:01 +0100

    hmtls hechos

--- src/main/java/com/example/demo2/controller/ProyectoController.java
+++ src/main/java/com/example/demo2/controller/ProyectoController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/admin/experiencia")
+@RequestMapping("/admin/proyecto")
 public class ProyectoController {
 
     @Autowired
